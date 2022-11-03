eaca7223c91e6d946471c32e1939d53a62a27734
Andrew Southall <andrew@trulydigital.net>
2022-11-03 10:58:43 +0000

    testing

--- cicd/manualTest.sh
+++ cicd/manualTest.sh
@@ -4,5 +4,5 @@ set -e
 
 # docker build -t smasherofallthings/$1:amd64-latest $1/
 
-docker buildx build -t smasherofallthings/$1:arm7-latest -f $1/Dockerfile --platform linux/arm/v7 --output type=docker $1
+docker buildx build -t smasherofallthings/$1:arm7-latest -f "$1/${ArmDockerfile:-Dockerfile}" --platform linux/arm/v7 --output type=docker $1
 
