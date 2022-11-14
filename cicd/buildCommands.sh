9c3d7cd2a4f008c5c707aef146fa1b85a8e80f36
Andrew Southall <andrew@trulydigital.net>
2022-11-14 21:03:51 +0000

    Turning off arm builds for python-fe, cryptography package taking an age

--- cicd/buildCommands.sh
+++ cicd/buildCommands.sh
@@ -2,14 +2,31 @@
 
 set -e
 
+allBuilds=()
+allBuilds+=('amd64-latest')
+allBuilds+=("amd64-$(date '+%Y-%m-%d')-b${DRONE_BUILD_NUMBER}")
+
+if [[ ! -z $SkipArmBuild ]] && [[ $SkipArmBuild == "true" ]]; then
+  printf "Skipping arm builds"
+else
+  allBuilds+=('arm7-latest')
+  allBuilds+=("arm7-$(date '+%Y-%m-%d')-b${DRONE_BUILD_NUMBER}")
+fi
+
 docker build -t "smasherofallthings/${IMAGE_NAME}:amd64-latest" -t "smasherofallthings/${IMAGE_NAME}:amd64-$(date '+%Y-%m-%d')-b${DRONE_BUILD_NUMBER}" "${IMAGE_NAME}/"
 
-docker buildx build -t "smasherofallthings/${IMAGE_NAME}:arm7-latest" -t "smasherofallthings/${IMAGE_NAME}:arm7-$(date '+%Y-%m-%d')-b${DRONE_BUILD_NUMBER}" -f "${IMAGE_NAME}/${ArmDockerfile:-Dockerfile}" --platform linux/arm/v7 --output type=docker "${IMAGE_NAME}"
+if [[ -z $SkipArmBuild ]]; then
+  docker buildx build -t "smasherofallthings/${IMAGE_NAME}:arm7-latest" -t "smasherofallthings/${IMAGE_NAME}:arm7-$(date '+%Y-%m-%d')-b${DRONE_BUILD_NUMBER}" -f "${IMAGE_NAME}/${ArmDockerfile:-Dockerfile}" --platform linux/arm/v7 --output type=docker "${IMAGE_NAME}"
+fi
 
-for f in 'amd64-latest' 'arm7-latest' "amd64-$(date '+%Y-%m-%d')-b${DRONE_BUILD_NUMBER}" "arm7-$(date '+%Y-%m-%d')-b${DRONE_BUILD_NUMBER}";
-  do printf "Pushing $f\n" && docker push "smasherofallthings/${IMAGE_NAME}:${f}"
+for f in "${allBuilds[@]}"; do
+  printf "Pushing $f\n" && docker push "smasherofallthings/${IMAGE_NAME}:${f}"
 done
 
-docker manifest create "smasherofallthings/${IMAGE_NAME}:latest" --amend "smasherofallthings/${IMAGE_NAME}:arm7-latest" --amend "smasherofallthings/$IMAGE_NAME:amd64-latest"
+if [[ -z $SkipArmBuild ]]; then
+  docker manifest create "smasherofallthings/${IMAGE_NAME}:latest" --amend "smasherofallthings/${IMAGE_NAME}:arm7-latest" --amend "smasherofallthings/$IMAGE_NAME:amd64-latest"
+else
+  docker manifest create "smasherofallthings/${IMAGE_NAME}:latest" --amend "smasherofallthings/$IMAGE_NAME:amd64-latest"
+fi
 
 docker manifest push "smasherofallthings/${IMAGE_NAME}:latest"
