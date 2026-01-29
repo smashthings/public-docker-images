ccfcd89efc432c7ea47a8024326346fdddafc00f
Andrew greybox <andrew@trulydigital.net>
2026-01-29 18:16:17 +0000

    amd64 only builds

--- cicd/drone/dockerCompose.sh
+++ cicd/drone/dockerCompose.sh
@@ -17,10 +17,6 @@ fi
 printf "${hr}\nBeginning build for ${IMAGE_NAME}...\n"
 cd "${IMAGE_NAME}"
 
-if [[ ! -z $SkipArmBuild ]] && [[ $SkipArmBuild == "true" ]]; then
-  printf "${hr}\nSkipping arm builds...\n"
-fi
-
 printf "${hr}\nBuilding x86 with build tag...\n"
 Tag="$(date '+%Y-%m-%d')-b${DRONE_BUILD_NUMBER}" docker compose build amd64
 printf "${hr}\nPushing x86 build tag...\n"
@@ -30,17 +26,8 @@ docker compose build amd64
 printf "${hr}\nPushing x86 latest...\n"
 docker compose push amd64
 
-if [[ -z $SkipArmBuild ]]; then
-  printf "${hr}\nBuilding arm latest...\n"
-  Tag="$(date '+%Y-%m-%d')-b${DRONE_BUILD_NUMBER}" docker compose build arm7
-  printf "${hr}\nPushing arm build tag...\n"
-  Tag="$(date '+%Y-%m-%d')-b${DRONE_BUILD_NUMBER}" docker compose push arm7
-  printf "${hr}\nCreating dual manifest for latest...\n"
-  docker manifest create "smasherofallthings/${IMAGE_NAME}:latest" --amend "smasherofallthings/${IMAGE_NAME}:arm7-latest" --amend "smasherofallthings/$IMAGE_NAME:amd64-latest"
-else
-  printf "${hr}\nCreating singular manifest for latest...\n"
-  docker manifest create "smasherofallthings/${IMAGE_NAME}:latest" --amend "smasherofallthings/$IMAGE_NAME:amd64-latest"
-fi
+printf "${hr}\nCreating singular manifest for latest...\n"
+docker manifest create "smasherofallthings/${IMAGE_NAME}:latest" --amend "smasherofallthings/$IMAGE_NAME:amd64-latest"
 
 printf "${hr}\nPushing manifest...\n"
 docker manifest push "smasherofallthings/${IMAGE_NAME}:latest"
